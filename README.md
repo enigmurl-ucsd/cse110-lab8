bc78a29bfc4588714282bf5d1863b8d2958f7aa3
enigmurl <43832426+enigmurl@users.noreply.github.com>
2024-05-22 11:04:16 -0700

    Finish lab

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Lab8-Starter
+
+## Partners
+Manu Bhat
+
+## URL
+[pages site](https://enigmurl-ucsd.github.io/cse110-lab8)
+
+## Graceful Degradation and Service Workers
+Graceful degradation and service workers are related because service workers can be used to help implement graceful degradation features. Graceful degradation refers to the design principle where an application maintains limited functionality even when parts of it fail or certain resources are unavailable. Service workers play a key role in this by managing network requests and caching assets, which ensures that essential parts of the application remain functional even without a network connection. In particular, rather than an application completely breaking without network, only minimal features that absolutely require the network can be disabled, whereas the main crux of the application can still use service workers to provide something of use. Therefore, service workers are an integral part (but not the sole method) to provide accesibility to all users in the topic of graceful degradation.
+
+## PWA
+
+![pwa](pwa.png)
--- sw.js
+++ sw.js
@@ -9,7 +9,14 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      return cache.addAll([
+			  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+			  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+			  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+			  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+			  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+			  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+      ]);
     })
   );
 });
@@ -21,20 +28,34 @@ self.addEventListener('activate', function (event) {
 
 // Intercept fetch requests and cache them
 self.addEventListener('fetch', function (event) {
-  // We added some known URLs to the cache above, but tracking down every
-  // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
-  // list to the cache as they come in.
-  /*******************************/
-  // This article from Google will help with this portion. Before asking ANY
-  // questions about this section, read this article.
-  // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
-  // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+	// We added some known URLs to the cache above, but tracking down every
+	// subsequent network request URL and adding it manually would be very taxing.
+	// We will be adding all of the resources not specified in the intiial cache
+	// list to the cache as they come in.
+	/*******************************/
+	// This article from Google will help with this portion. Before asking ANY
+	// questions about this section, read this article.
+	// NOTE: In the article's code REPLACE fetch(event.request.url) with
+	//       fetch(event.request)
+	// https://developer.chrome.com/docs/workbox/caching-strategies-overview/
+	/*******************************/
+	// B7. TODO - Respond to the event by opening the cache using the name we gave
+	//            above (CACHE_NAME)
+	event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+		// Respond with the image from the cache or from the network
+		return cache.match(event.request).then((cachedResponse) => {
+			// B8. TODO - If the request is in the cache, return with the cached version.
+			//            Otherwise fetch the resource, add it to the cache, and return
+			//            network response.
+			return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+				// Add the network response to the cache for future visits.
+				// Note: we need to make a copy of the response to save it in
+				// the cache and use the original as the request response.
+				cache.put(event.request, fetchedResponse.clone());
+
+				// Return the network response
+				return fetchedResponse;
+			});
+		});
+	}));
 });
